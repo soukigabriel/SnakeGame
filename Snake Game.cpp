73942d49421ba6f02fead3d8feb3fc129cdd8600
Gabriel Souki <gabrielsouki@gmail.com>
2022-11-24 10:56:14 -0400

    Added the class GameMap

--- Snake Game.cpp
+++ Snake Game.cpp
@@ -1,9 +1,21 @@
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
+	//for (int i = 0; i < 20; i++)
+	//{
+	//	cout << i << endl;
+	//	Sleep(1000);
+	//}
+
 
 
 	return 0;
